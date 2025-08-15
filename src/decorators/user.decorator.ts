40ce3b8b89b1f083e4fb167187656e62ae778aac
Adam Fuzm <fuzumoe@gmail.com>
2025-08-15 03:27:06 +0200

    style(auth): organize imports in auth service
    - Reorder imports alphabetically
    - Group imports by external and internal dependencies
    - Improve code organization and readability

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,23 +1,16 @@
+import { RequestWithUser } from '@auth/interfaces/auth.interface';
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { Request } from 'express';
 import { User } from '../entities/user.entity';
 
-export interface RequestWithUser extends Request {
-    user: Partial<User>;
-}
-
 export const CurrentUser = createParamDecorator((data: unknown, ctx: ExecutionContext): Partial<User> => {
-    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
-    return request.user || {};
+    const request: RequestWithUser = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    return user;
 });
 
-export const UserProperty = createParamDecorator((property: string, ctx: ExecutionContext): unknown => {
-    const request = ctx.switchToHttp().getRequest<RequestWithUser>();
+export const UserProperty = createParamDecorator((property: string, ctx: ExecutionContext): any => {
+    const request: RequestWithUser = ctx.switchToHttp().getRequest();
     const user = request.user;
 
-    if (!user) {
-        return undefined;
-    }
-
-    return property && property in user ? user[property as keyof User] : user;
+    return property ? user?.[property] : user;
 });
